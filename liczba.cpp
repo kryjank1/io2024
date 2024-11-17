15eacdb7a67faf25c22a53d691c763132a90d511
Krystian Jankun <kryjank1@mat.umk.pl>
2024-11-17 23:42:07 +0100

    dodano funkce czy_kwadratowa()

--- liczba.cpp
+++ liczba.cpp
@@ -1,8 +1,15 @@
 #include <iostream>
 
+bool czy_bezkwadratowa(int x) {
+	for(int i = 2; i*i < x; i++) {
+		if(x%(i*i)==0) return false;
+	}
+	return true;
+}
+
 int main() {
 	int x;
 	std::cin >> x;
-	std::cout << x << std::endl;
+	std::cout << x << (czy_bezkwadratowa(x) ? " Jest doskonala" : " Nie jest doskonala") << std::endl;
 	return 0;
 }
